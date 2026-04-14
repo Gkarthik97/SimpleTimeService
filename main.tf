4a364855704d9f792d3f838b7a337e9d6783a76a
Gkarthik97 <karthikeyudugalla@gmail.com>
2026-04-14 19:41:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,21 +26,35 @@ resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 }
 
-# ---------------- PUBLIC SUBNET (ALB) ----------------
-resource "aws_subnet" "public" {
+# ---------------- PUBLIC SUBNET 1 (ALB - AZ1) ----------------
+resource "aws_subnet" "public_1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "public-subnet"
+    Name = "public-subnet-1"
+  }
+}
+
+# ---------------- PUBLIC SUBNET 2 (ALB - AZ2) ----------------
+resource "aws_subnet" "public_2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-2"
   }
 }
 
 # ---------------- PRIVATE SUBNET (EC2) ----------------
 resource "aws_subnet" "private" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1a"
 
   tags = {
     Name = "private-subnet"
@@ -57,8 +71,13 @@ resource "aws_route_table" "public_rt" {
   }
 }
 
-resource "aws_route_table_association" "public_assoc" {
-  subnet_id      = aws_subnet.public.id
+resource "aws_route_table_association" "public_1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_2" {
+  subnet_id      = aws_subnet.public_2.id
   route_table_id = aws_route_table.public_rt.id
 }
 
@@ -100,7 +119,7 @@ resource "aws_security_group" "app_sg" {
   }
 }
 
-# ---------------- EC2 INSTANCE (PRIVATE) ----------------
+# ---------------- EC2 (PRIVATE) ----------------
 resource "aws_instance" "app" {
   ami           = "ami-0ec10929233384c7f"
   instance_type = "t2.micro"
@@ -146,20 +165,23 @@ resource "aws_lb_target_group" "tg" {
   }
 }
 
-# ---------------- ATTACH EC2 TO TG ----------------
+# ---------------- ATTACH EC2 ----------------
 resource "aws_lb_target_group_attachment" "attach" {
   target_group_arn = aws_lb_target_group.tg.arn
   target_id        = aws_instance.app.id
   port             = 5000
 }
 
-# ---------------- APPLICATION LOAD BALANCER ----------------
+# ---------------- ALB ----------------
 resource "aws_lb" "alb" {
   name               = "app-alb"
   load_balancer_type = "application"
-  internal           = false
 
-  subnets         = [aws_subnet.public.id]
+  subnets = [
+    aws_subnet.public_1.id,
+    aws_subnet.public_2.id
+  ]
+
   security_groups = [aws_security_group.alb_sg.id]
 }
 
@@ -174,5 +196,4 @@ resource "aws_lb_listener" "listener" {
     target_group_arn = aws_lb_target_group.tg.arn
   }
 }
-
 
